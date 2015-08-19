7ef4dd7213ebff97314087511f51323fc062c063
ksrt12 <kazakov12stepan2012@live.ru>
2015-08-19 22:17:07 +0600

    add

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,7 +41,10 @@ TARGET_OTA_ASSERT_DEVICE := leon,h324,lge_h324
 
 # Kernel
 TARGET_KERNEL_CONFIG := muse6582_y50_l_defconfig
-TARGET_KERNEL_SOURCE := kernel/lge/h324
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-4.9-14.06/bin/arm-linux-gnueabihf-
+TARGET_KERNEL_SOURCE := kernel/lge/mt6582
+#TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-4.9-14.06/bin/arm-linux-gnueabihf-
+
+# Recovery
+TARGET_RECOVERY_FSTAB := device/huawei/u8950/rootdir/fstab.huawei
 
 
